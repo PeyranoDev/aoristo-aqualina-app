0fdbefcf83eb8b243f185a56371a090e0aceb29e
Ignacio Peyrano <impeyrano@mail.austral.edu.ar>
2025-04-15 12:58:25 -0300

    Firs Commit

---
+++ Data/Entities/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Vehicle
+    {
+        public Guid Id { get; set; }
+        public string Plate { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public bool IsParked { get; set; }
+
+        // Relación: Un vehículo puede tener muchas solicitudes
+        public List<VehicleRequest> Requests { get; set; }
+    }
+}
---
+++ Data/Entities/VehicleRequest.cs
@@ -0,0 +1,26 @@
+﻿using Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class VehicleRequest
+    {
+        public Guid Id { get; set; }
+
+        // Foreign key
+        public Guid VehicleId { get; set; }
+
+        // Relación
+        public Vehicle Vehicle { get; set; }
+
+        // Estado de la solicitud
+        public VehicleRequestStatusEnum Status { get; set; } 
+
+        public DateTime RequestedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
