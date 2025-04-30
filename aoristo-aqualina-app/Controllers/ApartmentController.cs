ddd95cfaf9cefe77cd85cd03d305f6b7a0a14078
Ignacio Peyrano <impeyrano@mail.austral.edu.ar>
2025-04-29 23:30:23 -0300

    feature: start with apartment controller

---
+++ aoristo-aqualina-app/Controllers/ApartmentController.cs
@@ -0,0 +1,17 @@
+﻿using Common.Models.Requests;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aoristo_aqualina_app.Controllers
+{
+    [Route("apartments")]
+    [ApiController]
+    public class ApartmentController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateApartment([FromBody] ApartmentForCreateDTO dto)
+        {
+            return Ok();
+        }
+    }
+}
