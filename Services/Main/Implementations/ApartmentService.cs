a62746a2116e6486d5d3bac35618fa31f353a1c1
PeryanoDev <impeyrano@mail.austral.edu.ar>
2025-06-02 19:00:47 -0300

    feature: apartment creation and get list

---
+++ Services/Main/Implementations/ApartmentService.cs
@@ -0,0 +1,65 @@
+﻿using Common.Models.Requests;
+using Data.Entities;
+using Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Main.Implementations
+{
+    public class ApartmentService
+    {
+        private readonly IApartmentRepository _apartmentRepository;
+
+        public ApartmentService(IApartmentRepository apartmentRepository)
+        {
+            _apartmentRepository = apartmentRepository;
+        }
+
+        public async Task<Apartment> CreateApartmentAsync(ApartmentForCreateDTO apartment)
+        {
+            if (await _apartmentRepository.IdentifierExistsAsync(apartment.Identifier))
+            {
+                throw new InvalidOperationException("An apartment with this identifier already exists.");
+            }
+
+            var newApartment = new Apartment
+            {
+                Identifier = apartment.Identifier,
+                IsActive = true
+            };
+
+            return await _apartmentRepository.CreateAsync(newApartment);
+
+        }
+
+        public async Task<List<Apartment>> GetAllApartmentsAsync()
+        {
+            return await _apartmentRepository.GetApartmentsAsync();
+        }
+
+        public async Task<Apartment?> GetApartmentByIdAsync(int id)
+        {
+            return await _apartmentRepository.GetByIdAsync(id);
+        }
+        public async Task<Apartment?> UpdateApartmentAsync(Apartment apartment)
+        {
+            if (apartment.Id <= 0)
+            {
+                throw new ArgumentException("Invalid apartment ID.");
+            }
+
+            var existingApartment = await _apartmentRepository.GetByIdAsync(apartment.Id);
+            if (existingApartment == null)
+            {
+                throw new KeyNotFoundException("Apartment not found.");
+            }
+
+            return await _apartmentRepository.UpdateAsync(apartment);
+        }
+
+
+    }
+}
--- aoristo-aqualina-app/Controllers/ApartmentController.cs
+++ aoristo-aqualina-app/Controllers/ApartmentController.cs
@@ -1,17 +1,49 @@
 ﻿using Common.Models.Requests;
 using Data.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Services.Main.Implementations;
 
 namespace aoristo_aqualina_app.Controllers
 {
-    [Route("apartments")]
+    [Route("apartment")]
     [ApiController]
     public class ApartmentController : ControllerBase
     {
+        private readonly ApartmentService _apartmentService;
+        public ApartmentController(ApartmentService apartmentService)
+        {
+            _apartmentService = apartmentService;
+        }
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> CreateApartment([FromBody] ApartmentForCreateDTO dto)
         {
-            return Ok();
+            try
+            { 
+                var apartment = await _apartmentService.CreateApartmentAsync(dto);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
+        [HttpGet]
+        [Authorize(Roles = "Admin,Security")]
+        public async Task<IActionResult> GetAllApartments()
+        {
+            try
+            {
+                var apartments = await _apartmentService.GetAllApartmentsAsync();
+                return Ok(apartments);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+
     }
 }
