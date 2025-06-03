2d1926e44b2fd7df23ffe72584bb55d4a13635f7
Ignacio Peyrano <impeyrano@mail.austral.edu.ar>
2025-06-03 15:49:19 -0300

    feature: UserForCreateDTO to User AutoMapper set up

--- Common/Models/Profiles/UserProfile.cs
+++ Common/Models/Profiles/UserProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Common.Models;
+using Common.Models.Requests;
 using Common.Models.Responses;
 using Data.Entities;
 
@@ -16,5 +17,10 @@ public class UserProfile : Profile
                     Id = src.Apartment.Id,
                     Identifier = src.Apartment.Identifier
                 }));
+
+        CreateMap<UserForCreateDTO, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.Role, opt => opt.Ignore())
+            .ForMember(dest => dest.Apartment, opt => opt.Ignore());
     }
 }
